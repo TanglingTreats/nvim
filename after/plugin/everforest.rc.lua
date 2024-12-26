5bec178e971b8fe5365870164cabb39625bd2d61
TanglingTreats <foolstacked@gmail.com>
2024-12-27 00:31:50 +0800

    chore: update tsserver to ts_ls.

--- after/plugin/everforest.rc.lua
+++ after/plugin/everforest.rc.lua
@@ -9,7 +9,7 @@ everforest.setup({
   ---components be transparent (e.g. status line background)
   transparent_background_level = 2,
   ---Whether italics should be used for keywords and more.
-  italics = false,
+  italics = true,
   ---Disable italic fonts for comments. Comments are in italics by default, set
   ---this to `true` to make them _not_ italic!
   disable_italic_comments = false,
