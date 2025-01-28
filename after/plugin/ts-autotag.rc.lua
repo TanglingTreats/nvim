b0b30e420aad198662699e91c8db5a94dba0d55f
TanglingTreats <foolstacked@gmail.com>
2025-01-29 00:18:58 +0800

    chore: fix ts-autotag update issue. move duplicated configurations

--- after/plugin/ts-autotag.rc.lua
+++ after/plugin/ts-autotag.rc.lua
@@ -2,5 +2,32 @@ local status, autotag = pcall(require, 'nvim-ts-autotag')
 if (not status) then return end
 
 autotag.setup {
-
+  highlight = {
+    enable = true,
+    disable = {}
+  },
+  indent = {
+    enable = true,
+    disable = {}
+  },
+  ensure_installed = {
+    'vimdoc',
+    'lua',
+    'go',
+    'java',
+    'rust',
+    'javascript',
+    'html',
+    'c',
+    'tsx',
+    'cpp',
+    'c_sharp',
+    'json',
+    'css',
+    'python',
+    'yaml'
+  },
+  autotag = {
+    enable = true,
+  }
 }
